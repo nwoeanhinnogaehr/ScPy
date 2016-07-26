647c28e595b7647e1af4b48e5ec454079a98cc20
Noah Weninger <nweninge@ualberta.ca>
2016-07-25 19:54:12 -0600

    comments and a backbuffer

--- py/api.py
+++ py/api.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def out(dst, src):
+    """
+    Write the data in src to the buffer dst.
+    """
     if type(dst) is list:
         for i in range(0, len(dst)):
             dst[i][:] = src[i]
@@ -10,7 +13,43 @@ def out(dst, src):
         raise "invalid output type"
 
 def to_polar(x):
+    """
+    Convert from cartesian to polar coordinates, where the magnitude and
+    phase are stored as the real and imaginary parts of a complex number,
+    respectively.
+    """
     return np.abs(x) + 1j * np.angle(x)
 
 def from_polar(x):
+    """
+    Convert from polar to cartesian coordinates. The polar input is expected
+    to have its magnitude and phase stored as the real and imaginary parts
+    of a complex number, respectively.
+    """
     return x.real * np.exp(1j * x.imag)
+
+class BackBuffer:
+    """
+    A simple class that stores previous frames in a buffer.
+    """
+    def __init__(self, size):
+        self.size = size
+        self.items = []
+
+    def push(self, item):
+        """
+        Add an item to the end of the buffer.
+        If the size is exceeded, the last element will be removed.
+        """
+        if len(self.items) == self.size:
+            del self.items[0]
+        self.items.append(item)
+
+    def get(self, idx=-1):
+        """
+        Retrieve an item from the buffer at a given index.
+        Positive indices index forward in time, where as negative indices index
+        backward in time.
+        Indices outsize the bounds of the buffer will wrap around.
+        """
+        return self.items[idx % len(self.items)]
--- test.sc
+++ test.sc
@@ -2,7 +2,7 @@ s.boot; // run this line first
 
 // then, execute the block to perform setup.
 (
-    b = { Buffer.alloc(s,32768,1) }.dup;
+    b = { Buffer.alloc(s,4096,1) }.dup;
 
     // this runs the enclosed python code once,
     // to define the processing function.
@@ -10,10 +10,14 @@ s.boot; // run this line first
     // the block and the changes will be reflected
     // immediately.
     PyOnce("
+        global bb # this is necessary to make it visible between calls
+        bb = BackBuffer(64)
         def fn(x, time, mouse):
             x = to_polar(x)
             x.real = cos(x.real*mouse)*x.real # some weird operation
             x = from_polar(x)
+            bb.push(x)
+            x = bb.get(random.randint(0, 64))
             return x
     ");
 )
@@ -21,8 +25,8 @@ s.boot; // run this line first
 // finally, execute this block to start the synth.
 (
     {
-        var in = Saw.ar(8); // input is a 8Hz sawtooth wave
-        var chain = FFT(b.collect(_.bufnum), in);
+        var in = Saw.ar(32); // input is a sawtooth wave
+        var chain = FFT(b.collect(_.bufnum), in, hop:0.125);
         var mouse = MouseX.kr(1, 0);
         Py("
             out(x, fn(x, time, mouse))
